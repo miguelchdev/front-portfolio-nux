bc6994c0786f2cd73319e10e15e827a90ebfa15f
Miguel Chacon <miguelchdev@gmail.com>
2020-12-18 20:14:50 -0400

    load projects on created

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,46 @@
 <template>
-    <div>
-        <introduction
-            @click="go"
-            v-bind="introduction"
-        ></introduction>
+  <div>
+    <introduction
+      @click="go"
+      v-bind="introduction"
+    />
 
-        <service v-bind="services"></service>
+    <service v-bind="services" />
 
-        <project
-            id="project"
-            v-bind="projects"
-        ></project>
+    <project
+      id="project"
+      v-bind="projects"
+    />
 
-        <contact v-bind="contact"></contact>
-    </div>
+    <contact v-bind="contact" />
 </template>
 
 <script>
 import { mapState } from 'vuex'
 
 export default {
-    async asyncData({ store }) {
-        await store.dispatch('bio/fetchBio')
-        await store.dispatch('pages/fetchPages')
-        await store.dispatch('projects/fetchProjects')
+  async asyncData({ store }) {
+    await store.dispatch('bio/fetchBio')
+    await store.dispatch('pages/fetchPages')
+  },
+  computed: {
+    ...mapState('pages', ['services', 'contact', 'introduction', 'projects']),
+    ...mapState('bio', ['about']),
+    short_description() {
+      return this.about.slice(0, 120)
     },
-    computed: {
-        ...mapState('pages', [
-            'services',
-            'contact',
-            'introduction',
-            'projects',
-        ]),
-        ...mapState('bio', ['about']),
-        short_description() {
-            return this.about.slice(0, 120)
-        },
+  },
+  methods: {
+    go() {
+      this.$vuetify.goTo('#project', {
+        duration: 750,
+        offset: 0,
+        easing: 'easeInQuad',
+      })
     },
-    methods: {
-        go() {
-            this.$vuetify.goTo('#project', {
-                duration: 750,
-                offset: 0,
-                easing: 'easeInQuad',
-            })
-        },
-    },
+  },
+  async created() {
+    await store.dispatch('projects/fetchProjects')
+  },
 }
 </script>
